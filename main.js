dd102ce4d4afc9515c67911bd823923c11cc04f3
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-03-14 16:10:00 +0800

    Successfully pass reverseStr challenge.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+// Convert Temperature in degrees Celsius to Fahrenheit
 function convertToF(celsius) {
     document.getElementById("fahrenheit").innerHTML = (celsius * 9 / 5) + 32;
-    
+}
+
+// Reverse a String
+function reverseString(str) {
+    strArr = str.split("");
+    reversedArr = strArr.reverse();
+    reversedStr = reversedArr.join("");
+    document.getElementById("reversed").innerHTML = reversedStr;
 }
