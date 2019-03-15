12577275a15fcb8ee04e73766f5a07233abdd15d
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-03-15 10:38:45 +0800

    Successfully pass longestWordLength challenge.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function reverseString(str) {
     document.getElementById("reversed").innerHTML = reversedStr;
 }
 
-//Factorialize a Number
+// Factorialize a Number
 function factorialize(num) {
     if (num === 0 || num === 1) {
         return 1;
@@ -21,7 +21,27 @@ function factorialize(num) {
     }
 }
 
-document.getElementById("submit").addEventListener("click", function () {
+document.getElementById("factorialize").addEventListener("click", function () {
     num = document.getElementById("numberToFactorialize").value;
     document.getElementById("factorial").innerHTML = factorialize(num);
+});
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+    // split the string into an array of words
+    wordArray = str.split(" ");
+    // set first word as longest.
+    longest = wordArray[0];
+    // loop through the array to search for the longest word.
+    for (i = 1; i < wordArray.length; i++) {
+        if (wordArray[i].length > longest.length) {
+            longest = wordArray[i];
+        }
+    }
+    return ('The longest word is \"' + longest + '\" and its length is ' + longest.length + '.');
+}
+
+document.getElementById("findLongest").addEventListener("click", function () {
+    str = document.getElementById("stringGiven").value;
+    document.getElementById("longestWord").innerHTML = findLongestWordLength(str);
 });
