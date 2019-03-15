0446ed05de3be45b03fab7b8e0d316b90c97141a
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-03-15 14:33:22 +0800

    Successfully pass returnLargestNumbers challenge.

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ document.getElementById("findLongest").addEventListener("click", function () {
     str = document.getElementById("stringGiven").value;
     document.getElementById("longestWord").innerHTML = findLongestWordLength(str);
 });
+
+function largestOfFour(arr) {
+    var arrayOfLargest = [];
+    for (var i = 0; i < arr.length; i++) {
+      var largest = arr[i][0];
+      for (var j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] > largest) {
+          largest = arr[i][j]; 
+        }
+      }
+      arrayOfLargest.push(largest);
+    }
+    
+    return ('[' + arrayOfLargest + ']');
+  }
+  
+//    largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+  document.getElementById("returnLargestNumbers").addEventListener("click", function () {
+    // arr = document.getElementById("arrayGiven").value;
+    document.getElementById("arrayOfLargest").innerHTML = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+});
